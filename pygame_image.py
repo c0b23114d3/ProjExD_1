75754e19c8ce52e6663246ee167587dc48dc49cd
c0b23114d3 <c0b23114d3@edu.teu.ac.jp>
2024-06-11 16:54:31 +0900

    commit1-8

--- pygame_image.py
+++ pygame_image.py
@@ -10,16 +10,35 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False)
     koukaton = pg.image.load("fig/3.png")
     koukaton = pg.transform.flip(koukaton, True, False)
+    kkrect = koukaton.get_rect()
+    kkrect.center = 300, 200
+
     tmr = 0
-    img_x = tmr % 800
+    
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+        img_x = tmr % 3200
         screen.blit(bg_img, [-img_x, 0])
-        screen.blit(koukaton, [300, 200])
+        screen.blit(bg_img2, [-img_x + 1600, 0])
+        screen.blit(bg_img, [-img_x + 3200, 0])
+        screen.blit(bg_img2, [-img_x + 4800, 0])
+
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kkrect.move_ip((0, -1))
+        if key_lst[pg.K_DOWN]:
+            kkrect.move_ip((0, +1))
+        if key_lst[pg.K_LEFT]:
+            kkrect.move_ip((-1, 0))
+        if key_lst[pg.K_RIGHT]:
+            kkrect.move_ip((+1, 0))
+        screen.blit(koukaton, kkrect.center)
+        
         pg.display.update()
         tmr += 1        
         clock.tick(200)
